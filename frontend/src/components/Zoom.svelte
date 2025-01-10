03690c965ab30756e2e14c7bee175acfa7a35c10
qt <quentin@qskousen.info>
2025-01-10 07:54:45 -0600

    Fix for image zooming

--- frontend/src/components/Zoom.svelte
+++ frontend/src/components/Zoom.svelte
@@ -1,32 +1,25 @@
 <script lang="ts">
     import { onMount } from "svelte"
-    import {ZoomInSolid} from "flowbite-svelte-icons";
     import Zoomist from "zoomist";
 
     export let image: string;
-    let container: HTMLDivElement;
-    let currentPercentage: number;
+    let imageElement: HTMLDivElement;
 
     onMount(() => {
-        new Zoomist('.zoomist-container', {
-            // Optional parameters
-            maxScale: 4,
-            bounds: true,
-            // if you need slider
-            slider: true,
-            // if you need zoomer
-            zoomer: true
-        })
+        imageElement.onload = () => {
+            new Zoomist('.zoomist-container', {
+                maxScale: 8,
+                bounds: true,
+                slider: true,
+                zoomer: true
+            })
+        };
     });
 </script>
-<!-- zoomist-container -->
 <div class="zoomist-container">
-    <!-- zoomist-wrapper is required -->
     <div class="zoomist-wrapper">
-        <!-- zoomist-image is required -->
         <div class="zoomist-image w-full h-full">
-            <!-- you can add anything you want to zoom here. -->
-            <img src={image} />
+            <img bind:this={imageElement} src={image} class="w-full h-full" />
         </div>
     </div>
 </div>
